69597c18b5f90d3b3b3de8aa19d85e1c6ef17ff9
imamori <aidy.91614@gmail.com>
2014-10-23 11:43:54 +0900

    不要ファイルの削除

--- test.c
+++
@@ -1,5 +0,0 @@
-#include <stdio>
-int main(void){
-  int hoge1 = 0;
-  int hoge2 = 0;
-}
--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main(void){
-  int i = 0;
-  i = i + 1;
-  cout << "Hello World! " << i << endl;
-}
